cd6b9b8e705ee76f6b9ac2a28dbe9af13843ed23
Justin Grabowski <justingrabowski@yahoo.com>
2018-11-18 16:11:05 -0500

    first commit

--- 355_Ass1_Problem5/main.cpp
+++ 355_Ass1_Problem5/main.cpp
@@ -5,11 +5,16 @@
 //  Created by Justin Grabowski on 11/17/18.
 //  Copyright © 2018 Justin Grabowski. All rights reserved.
 //
+// This program tests operand evaluation order
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+   
+    
+    
+    
+    
+    
     return 0;
 }
